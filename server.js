b43413fca9d68ddbbec84c82cc603bcbc725f01f
matthew <matthkang@gmail.com>
2023-09-02 15:17:37 -0700

    impl promise and continuing ask() method

--- server.js
+++ server.js
@@ -58,6 +58,49 @@ const role_questions = [
     }
 ];
 
+function addRole() {
+    inquirer.prompt(role_questions).then((answers) => {
+        const sql = `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`;
+        const departmentId = {
+            Engineering: '1',
+            Finance: '2',
+            Legal: '3',
+            Sales: '4',
+            Service: '5'
+        }
+        const roleName = answers.role_name;
+        const salary = answers.salary;
+        const department = answers.department;
+
+        const params = [roleName, salary, departmentId[department]];
+
+        db.promise().query(sql, params, (err, result) => {
+            if (err) {
+                console.log(err);
+            }
+        })
+        .catch(console.log)
+        .then(() => {
+            db.end();
+            ask();
+        })
+    })
+}
+
+function viewDepartments() {
+    const sql = `SELECT * FROM department`;
+    db.promise().query(sql)
+        .then(([rows, fields]) => {
+            console.log();
+            console.table(rows);
+        })
+        .catch(console.log)
+        .then(() => {
+            db.end();
+            ask();
+        });
+}
+
 function ask() {
     inquirer.prompt(questions)
         .then((answers) => {
@@ -68,46 +111,20 @@ function ask() {
             else if (answers.choice === 'Update Employee Role') {
             }
             else if (answers.choice === 'View All Roles') {
+                
             }
             else if (answers.choice === 'Add Role') {
-                inquirer.prompt(role_questions).then((answers) => {
-                    const sql = `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`;
-                    const departmentId = {
-                        Engineering: '1',
-                        Finance: '2',
-                        Legal: '3',
-                        Sales: '4',
-                        Service: '5'
-                    }
-                    const roleName = answers.role_name;
-                    const salary = answers.salary;
-                    const department = answers.department;
-
-                    const params = [roleName, salary, departmentId[department]];
-
-                    db.query(sql, params, (err, result) => {
-                        if (err) {
-                            console.log(err);
-                        }
-                    });
-                })
+                addRole();
             }
             else if (answers.choice === 'View All Departments') {
-                const sql = `SELECT * FROM department`;
-                db.promise().query(sql)
-                .then(([rows,fields]) => {
-                    console.log();
-                    console.table(rows);
-                })
-                .catch(console.log)
-                .then(() => db.end());
+                viewDepartments();
             }
             else if (answers.choice === 'Add Department') {
-                
+
             }
             else if (answers.choice === 'Quit') {
             }
-            ask();
+
         })
 }
 
@@ -120,8 +137,8 @@ function init() {
    |_____|_| |_| |_| .__/|_|\\___/ \\__, |\\___|\\___| |_|  |_|\\__,_|_| |_|\\__,_|\\__, |\\___|_|   
                    |_|            |___/                                      |___/           
    `;
-   console.log(welcome_message);
-   ask();
+    console.log(welcome_message);
+    ask();
 }
 
 init();
